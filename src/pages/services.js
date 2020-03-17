09757d10650dcd9e3a6c7b5056f196e2c4d9e03f
Luke Bennett <hello@lukebennett.com.au>
2020-03-18 10:10:16 +1100

    Add extra pages

---
+++ src/pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+export default function ServicesPage() {
+  return (
+    <Layout>
+      <SEO title="Services" />
+    </Layout>
+  );
+}
