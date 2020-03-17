9ed9d6822e42e92b306644b59e178030a0f7c64d
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2020-03-17 15:08:06 +1100

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -37,26 +37,57 @@ export default function Footer() {
     site: { siteMetadata },
   } = useGraphQL();
   return (
-    <div className="bg-white">
-      <div className="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div className="flex justify-center md:order-2">
-          {social.map((socialNetwork, index) => (
-            <a
-              key={index}
-              href={socialNetwork.link}
-              className="ml-6 text-gray-400 hover:text-gray-500 first:ml-0"
-            >
-              <span className="sr-only">{socialNetwork.network}</span>
-              <socialNetwork.network className="w-6 h-6" />
-            </a>
-          ))}
+    <div className="bg-black">
+      <div className="flex items-center justify-center max-w-screen-xl px-4 py-12 mx-auto md:justify-between sm:px-6 lg:px-8">
+        <div className="hidden leading-none md:block">
+          <h2 className="text-6xl font-extrabold uppercase text-brand-500">
+            Quickii
+          </h2>
+          <h3 className="pl-12 text-4xl font-bold text-white">Construction.</h3>
         </div>
-        <div className="mt-8 md:mt-0 md:order-1">
-          <p className="text-base leading-6 text-center text-gray-400">
-            &copy; 2020 {siteMetadata.title}. All rights reserved.
-          </p>
+        <div className="md:order-2">
+          <div className="flex flex-col">
+            <div className="flex justify-end">
+              {social.map((socialNetwork, index) => (
+                <a
+                  key={index}
+                  href={socialNetwork.link}
+                  className="ml-2 text-gray-400 hover:text-gray-500 first:ml-0"
+                >
+                  <span className="sr-only">{socialNetwork.network}</span>
+                  <socialNetwork.network className="w-6 h-6 bg-transparent rounded-full text-brand-600" />
+                </a>
+              ))}
+            </div>
+            <div className="mt-4 text-right">
+              <p className="text-xl font-extrabold text-brand-500">
+                Phone:{' '}
+                <span className="text-lg font-thin text-white">
+                  1-800-QUICKII-CO
+                </span>
+              </p>
+
+              <p className="text-xl font-extrabold text-brand-500">
+                Address:{' '}
+                <span className="text-lg font-thin text-white">
+                  23 Quickii St,
+                  <br /> Port Macquarie NSW 2444
+                </span>
+              </p>
+              <p className="text-xl font-extrabold text-brand-500">
+                Hours:{' '}
+                <span className="text-lg font-thin text-white">
+                  9AM - 5PM Monday-Friday
+                </span>
+              </p>
+            </div>
+          </div>
+          <div />
         </div>
       </div>
+      <p className="px-2 mb-4 text-base leading-6 text-center text-gray-400">
+        &copy; 2020 {siteMetadata.title}. All rights reserved.
+      </p>
     </div>
   );
 }
