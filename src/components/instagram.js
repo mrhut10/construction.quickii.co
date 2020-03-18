2f47118e64e5de5a07b77ebb16048ab93f3546dd
Luke Bennett <hello@lukebennett.com.au>
2020-03-18 11:42:04 +1100

    Remove data-src after image has loaded

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -69,6 +69,9 @@ function Image({ item, extraClasses }) {
   useEffect(() => {
     if (inView) {
       imgRef.current.src = imgRef.current.dataset.src;
+      setTimeout(() => {
+        imgRef.current.removeAttribute('data-src');
+      }, 1000);
     }
   }, [inView]);
 
