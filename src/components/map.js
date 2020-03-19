d0c697ced29f99b2d692153568d17d8a236d45bf
Luke Bennett <hello@lukebennett.com.au>
2020-03-20 10:23:03 +1100

    Lazy-load map

--- src/components/map.js
+++ src/components/map.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
+
 import { useGraphQL } from '../hooks';
 
 export default function Map() {
+  const [ref, inView] = useInView({
+    threshold: 0,
+    triggerOnce: true,
+  });
+
   const {
     site: { siteMetadata },
   } = useGraphQL();
+
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    if (inView) {
+      iframeRef.current.src = iframeRef.current.dataset.src;
+      setTimeout(() => {
+        iframeRef.current.removeAttribute('data-src');
+      }, 1000);
+    }
+  }, [inView]);
+
   return (
-    <div className="mt-6 overflow-hidden bg-white shadow sm:rounded-lg">
+    <div
+      ref={ref}
+      className="mt-6 overflow-hidden bg-white shadow sm:rounded-lg"
+      height={450}
+    >
       <iframe
-        src={siteMetadata.mapEmbed}
+        ref={iframeRef}
+        data-src={siteMetadata.mapEmbed}
         height={450}
         frameBorder={0}
         style={{ border: 0 }}
         allowFullScreen
         aria-hidden="false"
         title="Location"
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
         tabIndex={0}
-        className="w-full"
+        className="w-full bg-gray-100"
       />
     </div>
   );
